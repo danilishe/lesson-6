f147bd95a41df203280c14a9c5b13f59b8f951b1
danilishe <suetin.danil@gmail.com>
2020-07-02 22:24:46 +0400

    Test framework in project

--- src/org/example/SimpleTest.java
+++ src/org/example/SimpleTest.java
@@ -1,5 +1,13 @@
 package org.example;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SimpleTest {
-    // TODO add test library
+    @Test
+    public void testSumm() {
+        Assert.assertEquals("11 + 11 = 22", 22, 11 + 11);
+    }
+    // FIXME Test passes, but too fast! I don't understand what's happened!
+    // TODO Add logging
 }
