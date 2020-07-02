27f593cc8addd0dd0159f984922db93de1c37dec
danilishe <suetin.danil@gmail.com>
2020-07-02 23:27:45 +0400

    Clear maven structure

---
+++ src/test/java/org/example/SimpleTest.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SimpleTest {
+    private static final Logger log = LogManager.getLogger();
+    @Test
+    public void testSumm() {
+        log.info("Сейчас мы будем проверять сумму");
+        Assert.assertEquals("11 + 11 = 22", 22, 11 + 11);
+        log.info("Тест прошёл успешно!");
+    }
+    // Start something meaningful!
+}
