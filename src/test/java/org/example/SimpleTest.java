611376d31f267c653b3768c33bffec9aae81a3be
danilishe <suetin.danil@gmail.com>
2020-07-03 17:46:45 +0400

    Serialize with Gson

--- src/test/java/org/example/SimpleTest.java
+++ src/test/java/org/example/SimpleTest.java
@@ -2,6 +2,7 @@ package org.example;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.google.gson.Gson;
 import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
 import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 import org.apache.logging.log4j.LogManager;
@@ -63,4 +64,19 @@ public class SimpleTest {
         assertEquals(regularAutotester, deserializedAutotester);
     }
 
+
+    @Test
+    public void serializationWtihGson() throws JsonProcessingException {
+        Human cleverAutotester = new Human().setAge(22)
+                .setGender(Gender.FEMALE)
+                .setName("Виктория");
+        Gson gson = new Gson();
+        String cleverAtJson = gson.toJson(cleverAutotester);
+
+        log.info("AT JSON: " + cleverAtJson);
+
+        Human deserializedCleverAutotester = gson.fromJson(cleverAtJson, Human.class);
+
+        assertEquals(cleverAutotester, deserializedCleverAutotester);
+    }
 }
