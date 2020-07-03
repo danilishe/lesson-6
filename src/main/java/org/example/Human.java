51497299c372da60f82401545c0f4725380e3cfe
danilishe <suetin.danil@gmail.com>
2020-07-03 17:16:15 +0400

    Serialize with Serializable

---
+++ src/main/java/org/example/Human.java
@@ -0,0 +1,66 @@
+package org.example;
+
+import java.io.Serializable;
+
+public class Human implements Serializable {
+    String name;
+    int age;
+    Gender gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public Human setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Human)) return false;
+
+        Human human = (Human) o;
+
+        if (getAge() != human.getAge()) return false;
+        if (getName() != null ? !getName().equals(human.getName()) : human.getName() != null) return false;
+        return getGender() == human.getGender();
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getName() != null ? getName().hashCode() : 0;
+        result = 31 * result + getAge();
+        result = 31 * result + (getGender() != null ? getGender().hashCode() : 0);
+        return result;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Human setAge(int age) {
+        this.age = age;
+        return this;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public Human setGender(Gender gender) {
+        this.gender = gender;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                '}';
+    }
+
+}
--- src/test/java/org/example/SimpleTest.java
+++ src/test/java/org/example/SimpleTest.java
@@ -1,9 +1,15 @@
 package org.example;
 
+import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 public class SimpleTest {
     private static final Logger log = LogManager.getLogger();
@@ -13,5 +19,29 @@ public class SimpleTest {
         Assert.assertEquals("11 + 11 = 22", 22, 11 + 11);
         log.info("Тест прошёл успешно!");
     }
-    // Start something meaningful!
+
+    @Test
+    public void serializationWithSerializable() throws IOException, ClassNotFoundException {
+        Human vaisilyPupkin = new Human().setAge(10)
+                .setGender(Gender.MALE)
+                .setName("Vaisily Pupkin");
+
+        ByteOutputStream baos = new ByteOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(baos);
+        oos.writeObject(vaisilyPupkin);
+        oos.flush();
+        oos.close();
+
+        log.info(vaisilyPupkin + " after serialization " + baos.toString());
+
+        ByteInputStream bis = new ByteInputStream(baos.getBytes(), baos.getBytes().length);
+        ObjectInputStream ois = new ObjectInputStream(bis);
+        Human deserializedVasiliyPupkin = (Human) ois.readObject();
+        ois.close();
+        bis.close();
+
+        log.info("After deserialization: " + deserializedVasiliyPupkin);
+        Assert.assertEquals(vaisilyPupkin, deserializedVasiliyPupkin);
+    }
+
 }
